ee0575278c36c9bd5daa8042144bf9dd2cf0b2de
“Chainarin04” <684259042@webmail.npru.ac.th>
2025-07-25 15:23:01 +0700

    change input type to keyboard

--- SumTwoNumbers.java
+++ SumTwoNumbers.java
@@ -1,13 +1,19 @@
+import java.util.Scanner; // นำเข้า Scanner
+
 public class SumTwoNumbers {
     public static void main(String[] args) {
-        System.out.print("Enter first number: ");
+        Scanner input = new Scanner(System.in); // สร้าง Scanner เพื่อรับค่าจากผู้ใช้
+
+        System.out.print("Enter num 1: ");
         int num1 = input.nextInt();
 
-        System.out.print("Enter second number: ");
+        System.out.print("Enter num 2: ");
         int num2 = input.nextInt();
 
         int sum = num1 + num2;
 
         System.out.println("ผลรวมของ " + num1 + " และ " + num2 + " คือ " + sum);
+
+        input.close(); // ปิด Scanner เมื่อใช้งานเสร็จ
     }
 }
